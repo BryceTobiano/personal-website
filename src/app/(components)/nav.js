9af3e9d5c128779f87de884f8b8859d8ab46c671
Bryce Tobiano <tobiano@usc.edu>
2024-12-29 09:02:03 -0800

    Portfolio Page, Systolic Array Page, Navbar

--- src/app/(components)/nav.js
+++ src/app/(components)/nav.js
@@ -3,12 +3,13 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav>
+    <nav className={styles.nav}> 
         <div className={styles.logo}><Link href="/">BRYCE TOBIANO</Link></div>
         <div className={styles.links}>
-            <Link href="">About</Link>
-            <Link href="">Portfolio</Link>
-            <Link href="">LinkedIn</Link>
+            <Link href="/about">About</Link>
+            <Link href="/portfolio">Portfolio</Link>
+            <Link href="https://www.linkedin.com/in/bryce-tobiano">LinkedIn</Link>
+            <Link href="/BryceTobianoResume.pdf">Resume</Link>
         </div>
     </nav>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import styles from "./home.module.css";
 import Link from "next/link";
-import { StarFilledIcon, LinkedInLogoIcon, GitHubLogoIcon} from "@radix-ui/react-icons"
+import { StarFilledIcon, LinkedInLogoIcon, GitHubLogoIcon, FileTextIcon } from "@radix-ui/react-icons"
 
 export default function Home() {
   return (
@@ -12,20 +12,22 @@ export default function Home() {
         </div>
         
         <div className="p-text">
-          Hi! My name is Bryce. <p>I&apos;m tinkerer and engineer with interests in Embedded Software, Web Development, and more. 
+          Hi! My name is Bryce. 
+          <p>I&apos;m tinkerer and engineer with interests in Embedded Software, Web Development, and more. 
           Currently, I am at <b className={styles.uline}>USC</b> ✌️ studying <b className={styles.uline}>Computer Engineering and Computer Science (CECS)</b>.</p>
-          <p>When I&apos;m not working you can find me cooking, practicing for the Trojan Tetris Team, or snowboarding.</p>
+          <p>
+            When I&apos;m not working you can find me cooking, practicing for the&nbsp;
+            <Link href="https://www.usctrojanesports.com/teams/tetris" target="_blank" 
+            rel="noopener noreferrer">
+              <b className={styles.uline}>
+                 Trojan Tetris Team
+              </b>
+            </Link>
+          , or snowboarding.</p>
         </div>
 
         <div className={styles.ctas}>
-          {/* <Link 
-            href="/portfolio" 
-            rel="noopener noreferrer"
-            className={styles.primary}
-          >
-            <StarFilledIcon/>
-            <span>Portfolio</span>
-          </Link> */}
+          
           <Link 
             href="/BryceTobianoResume.pdf" 
             rel="noopener noreferrer"
@@ -35,7 +37,15 @@ export default function Home() {
             <span>Resume</span>
           </Link>
           <Link 
-            href="https://www.linkedin.com/in/brycetobiano" 
+            href="/portfolio" 
+            rel="noopener noreferrer"
+            className={styles.primary}
+          >
+            <FileTextIcon/>
+            <span>Portfolio</span>
+          </Link>
+          <Link 
+            href="https://www.linkedin.com/in/bryce-tobiano" 
             target="_blank" 
             rel="noopener noreferrer"
             className={styles.secondary}
@@ -52,6 +62,7 @@ export default function Home() {
             <GitHubLogoIcon/>
             <span>Github</span>
           </Link>
+
         </div>
       </main>
       <footer className={styles.footer}>&quot;And not only so, but we also boast in our tribulations, knowing that tribulation produces endurance&quot;&ensp;- Romans 5:3</footer>
